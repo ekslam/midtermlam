be972a7fc4aba076459522657073e76f8d558832
Kevin <edwardkevinl1@gmail.com>
2019-01-28 18:19:48 +0800

    manage done

--- post/models.py
+++ post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title= models.CharField(max_length=100)
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated')
+    content = models.TextField()
+    is_active = BooleanField()
+
+class Comment(models.Model):
+    date_created = models.DateTimeField('Date Created')
+    content = models.TextField()
+    post = models.ForeignKey('Post', on_delete = models.CASCADE)
+
+
+    #def __str__(self):
+    #    return 'Questions: {}'.format(self.question_text)
